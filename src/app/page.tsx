8f33baade24a9de9f97248880920780968db46c4
LEO <lethai2597@gmail.com>
2025-09-30 14:07:13 +0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { Presentation } from "lucide-react";
+import { MonitorCheck, Plus, Presentation } from "lucide-react";
 import Link from "next/link";
 import DownloadApp from "@/components/DownloadApp";
 
 export default function Home() {
-
   return (
     <div className="container mx-auto px-4 py-12">
       <div className="text-center mb-12">
@@ -13,7 +12,19 @@ export default function Home() {
         </p>
 
         {/* Download App Button */}
-        <DownloadApp />
+        <div className="flex justify-center gap-4 items-center">
+          <DownloadApp />
+
+          <a
+            className="inline-flex cursor-pointer items-center gap-2 px-4 py-2 bg-zinc-800 hover:bg-zinc-700 text-white rounded-full text-sm font-medium transition-colors duration-200 shadow-sm hover:shadow-md"
+            href="https://www.facebook.com/ko.ten.2571997/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Plus className="w-5 h-5" />
+            <span>Request new tool</span>
+          </a>
+        </div>
       </div>
 
       <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -21,11 +32,34 @@ export default function Home() {
           href="/slide-maker"
           className="bg-zinc-800 rounded-2xl p-8 flex items-center gap-8 hover:bg-zinc-700 transition-colors duration-200"
         >
-          <Presentation className="w-10 h-10" />
-          <h2 className="text-xl font-semibold text-zinc-100">
-            Simple Slide Maker
-          </h2>
+          <Presentation className="w-14 h-14" />
+          <div className="flex-1">
+            <h2 className="text-xl font-semibold text-zinc-100 mb-1">
+              Simple Slide Maker
+            </h2>
+            <div className="flex items-center gap-2 text-blue-400">
+              <div className="flex items-center gap-1">
+                <MonitorCheck className="w-4 h-4" />
+                <span className="text-sm">Desktop</span>
+              </div>
+            </div>
+          </div>
         </Link>
+
+        <a
+          href="https://www.facebook.com/ko.ten.2571997/"
+          className="bg-zinc-800 rounded-2xl p-8 flex items-center gap-8 hover:bg-zinc-700 transition-colors duration-200"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Plus className="w-14 h-14" />
+          <div className="flex-1">
+            <h2 className="text-xl font-semibold text-zinc-100 mb-1">
+              Request new tool
+            </h2>
+            <div className="text-sm">Contact me to request new tool</div>
+          </div>
+        </a>
       </div>
     </div>
   );
